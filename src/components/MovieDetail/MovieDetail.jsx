05fcc71e10a002e0f2568c24503881c1f2952160
Andrew Carey <andrew.r.carey@gmail.com>
2022-12-10 20:11:41 -0600

    brought in the current movie into the movie detail page and got the poster to display

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 
 export default function MovieDetail(props) {
+    const params = useParams()
+    // so, we have the ID of the movie we want to render in here. We need to select the data based on the 
+    const movies = useSelector(store => store.movies)
+
+    useEffect(
+        
+    )
+    
+    console.log(movies);
+    console.log(params.id);
+    const currentMovie = movies.find(movie => movie.id == params.id)
+    console.log(currentMovie);
+
     return (
-        <div>Detailed View of {props.movie.title}</div>
+        <div>
+            <img src={currentMovie.poster} alt="" />
+        </div>
     )
 }
