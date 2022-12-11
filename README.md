2a75aa9059059affd5f36bf5ad3b18c21524874e
Andrew Carey <andrew.r.carey@gmail.com>
2022-12-11 15:38:23 -0600

    attempted to get movie to stay loaded on refresh. Did not work

--- README.md
+++ README.md
@@ -9,11 +9,9 @@ Your project description goes here. What problem did you solve? How did you solv
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
 
 ## TODO
-[ ] GET list of genres along with the other info
+[√] GET list of genres along with the other info
 [ ] Create button that goes back to the movie page
 [ ] Style cards in selection page
 [ ] Style detail page
 [ ] update readme page
 
-
-## 
--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -7,23 +8,21 @@ export default function MovieDetail(props) {
     // we'll use params to identify which movie page we are on. This is in the URL
     const params = useParams()
     // we'll bring in the whole array of movies so that we can select which one we are looking at with the ID
-    const movies = useSelector(store => store.movies)
-    console.log(movies);
+    const [movies, setMovies] = useState(useSelector(store => store.movies))
     // the current movie is the movie that matches our params ID
-    const currentMovie = movies.find(thisMovie => thisMovie.movie.id == params.id).movie
-    console.log(currentMovie);
-    console.log('genres:', currentMovie.genres);
-    // We'll need to fetch the genres of each movie based o
+    const [currentMovie, setCurrentMovie] = useState(movies.find(thisMovie => thisMovie.movie.id == params.id).movie)
+
 
     return (
         <div>
             <h2>{currentMovie.title}</h2>
             <img src={currentMovie.poster} alt={currentMovie.title} />
             <p>{currentMovie.description}</p>
+            <h4>Genres:</h4>
             <ul>
                 {currentMovie.genres.map(genre => {
                         return (
-                            <li key = {genre.genres}>{genre.genres}</li>
+                            <div key = {genre.genres}>{genre.genres}</div>
                         )
                     }                    
                 )}
