72d6673d9ff8d43610dff3b20f62081312aad65d
Andrew Carey <andrew.r.carey@gmail.com>
2022-12-10 19:05:03 -0600

    added a component for movie detail that is routed to the movie ID in the url

---
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -0,0 +1,7 @@
+
+
+export default function MovieDetail(props) {
+    return (
+        <div>Detailed View of {props.movie.title}</div>
+    )
+}
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './MovieList.css'
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    const history = useHistory();
+
+    const handlePosterClick = (movie) => {
+        console.log(movie.title);
+        history.push(`/detail/${movie.id}`)
+        // on the click of each movie, load that movie into a global state? 
+    }
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
@@ -19,7 +27,10 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img 
+                                src={movie.poster} 
+                                alt={movie.title}
+                                onClick={() => handlePosterClick(movie)}/>
                         </div>
                     );
                 })}
